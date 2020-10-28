65692445fa1b16e84e3e9720ded9d2ca4a84eb58
khskeb0513 <khskeb0513@gmail.com>
2020-10-28 09:10:07 +0900

    remove dev data

--- routes/leases.js
+++ routes/leases.js
@@ -3,10 +3,7 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/', (req, res, next) => {
-    // let leases = fs.readFileSync('/var/lib/NetworkManager/dnsmasq-wlp2s0.leases', 'utf8').split('\n')
-    let leases = '1603846102 8c:a9:82:15:3f:e0 10.42.0.92 samentos 01:8c:a9:82:15:3f:e0\n' +
-        '1603845613 40:ec:99:ab:36:5f 10.42.0.195 DESKTOP-9NQHPMF 01:40:ec:99:ab:36:5f\n' +
-        '1603846120 f8:e6:1a:10:98:08 10.42.0.156 Galaxy-S7 01:f8:e6:1a:10:98:08'
+    let leases = fs.readFileSync('/var/lib/NetworkManager/dnsmasq-wlp2s0.leases', 'utf8').split('\n')
     leases = leases.split('\n')
     leases = leases.map(each_data => {
         each_data = each_data.split(' ')
